348a3fee5d92f26c8c129212f1051e3f08220dfa
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2021-01-29 12:24:57 +0100

    Cleanup code

--- public/scripts/languagesAndFrameworks.js
+++ public/scripts/languagesAndFrameworks.js
@@ -5,19 +5,23 @@ function fetchLanguagesAndFramworks(){
         .then(v => v.json())
         .then(response => {
 
-            response.forEach(element => {
-                let newDiv = document.createElement("div")
-                newDiv.className = "frameworkBox appear"
-                newDiv.setAttribute('onclick', 'openLanguageBox(this)')
-                newDiv.innerHTML = `
-                <img class="languageImage" src="${element.img}">
-                <h3 class="languageTitle">${element.title}</h3>
-                <p class="languageDiscriptionInvisible">${element.discription}</p>
-                `
-// {/* <a href="${element.url}" target="_blank"> <button class="projectButton">More info</button></a> */}
-                languageDiv.append(newDiv)
-            });
+            generateFrameworkGrid(response)
+            
         })
         .then(res => showElements())
 
+}
+
+function generateFrameworkGrid(response){
+    response.forEach(element => {
+        let newDiv = document.createElement("div")
+        newDiv.className = "frameworkBox appear"
+        newDiv.setAttribute('onclick', 'openLanguageBox(this)')
+        newDiv.innerHTML = `
+        <img class="languageImage" src="${element.img}">
+        <h3 class="languageTitle">${element.title}</h3>
+        <p class="languageDiscriptionInvisible">${element.discription}</p>
+        `
+        languageDiv.append(newDiv)
+    });
 }
